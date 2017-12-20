03c1cd0e4fece0162cb559aff8fd1fe3d08b4029
GucciBawler <34696410+GucciBawler@users.noreply.github.com>
2017-12-20 02:30:24 +0100

    first version

--- revc.py
+++ revc.py
@@ -4,4 +4,11 @@ REVC.py
 A script that generates the reverse complement of a string.
 '''
 
-for nucleic
+translation = {'A':'T', 'C':'G', 'G':'C', 'T':'A'}
+
+revc = str()
+
+for nucleic in input()[::-1]:
+	revc += translation[nucleic]
+	
+print(revc)
