029d2d2d7190fbe44e5c7f607e234c7edabca445
GucciBawler <34696410+GucciBawler@users.noreply.github.com>
2017-12-20 02:11:20 +0100

    First version. Could be written in a shortened loop.

---
+++ RNA.py
@@ -0,0 +1,15 @@
+'''
+RNA.py
+
+A script that translates a DNA string in RNA.
+'''
+
+RNA_string = str()
+
+for nucleic in input():
+	if nucleic == 'T':
+		RNA_string += 'U'
+	else:
+		RNA_string += nucleic
+		
+return(RNA_string)
