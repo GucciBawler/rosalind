f289d616e38e21f4093d8e7b8618157807ad7220
GucciBawler <34696410+GucciBawler@users.noreply.github.com>
2017-12-20 02:21:03 +0100

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,18 +1,18 @@
 '''
-DNA.py
+dna.py
 
 A script to count the occurence of the amino acids in a string of DNA.
 '''
 
-amino_count = {'A':0, 'C':0, 'G':0, 'T':0}
+nucleic_count = {'A':0, 'C':0, 'G':0, 'T':0}
 
-for amino in input():
-	amino_count[amino] += 1
+for nucleic in input():
+	nucleic_count[nucleic] += 1
 
 
 print_string = str()
 
-for count in amino_count:
-	print_string += str(amino_count[count])+' '
+for count in nucleic_count:
+	print_string += str(nucleic_count[count])+' '
 
 print(print_string.strip())
